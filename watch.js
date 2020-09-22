ef202802b6caed495aee895dd154a6353e8011dd
estrattonbailey <ericstrattonbailey@gmail.com>
2020-09-21 20:52:02 -0500

    remove fuzz files

--- watch.js
+++
@@ -1,9 +0,0 @@
-// require('chokidar').watch('.', { ignoreInitial: true }).on('all', (e, f) => {
-//   console.log(e, f)
-// })
-
-const instance = require('./')('./test/*.page.js')
-
-instance.on('update', files => {
-  console.log(files)
-})
